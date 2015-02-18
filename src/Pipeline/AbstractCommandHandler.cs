adcb9dc07d8d75200836a0c64b289e285a7ccc18
Raja Thapa <raja.thapa@comparethemarket.com>
2015-02-18 17:20:19 +0000

    moving files to correct place

---
+++ src/Pipeline/AbstractCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Pipeline
+{
+    public abstract class AbstractCommandHandler<TCommand> : ICommandHandler<TCommand, Nothing>
+    {
+        public Nothing Handle(TCommand command)
+        {
+            HandleCommand(command);
+            return Nothing.Value;
+        }
+
+        public abstract void HandleCommand(TCommand message);
+    }
+}
--- src/Pipeline/IPipeLine.cs
+++ src/Pipeline/IPipeLine.cs
@@ -5,15 +5,4 @@
         void Send<TCommand>(TCommand command) where TCommand : ICommand;
         TResult Send<TCommand, TResult>(TCommand command) where TCommand : ICommand;
     }
-
-    public abstract class AbstractCommandHandler<TCommand> : ICommandHandler<TCommand, Nothing>
-    {
-        public Nothing Handle(TCommand command)
-        {
-            HandleCommand(command);
-            return Nothing.Value;
-        }
-
-        public abstract void HandleCommand(TCommand message);
-    }
 }
