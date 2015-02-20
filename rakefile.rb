cc5cdfa7219d38b6cee639379cad5e253776f1fa
Raja Thapa <raja.thapa@comparethemarket.com>
2015-02-20 16:53:44 +0000

    added build task in rake

--- rakefile.rb
+++ rakefile.rb
@@ -1,9 +1,10 @@
 require 'rake/clean'
+require 'albacore'
 
 @build_dir = 'build'
 
 CLEAN.include(@build_dir)
-task :default => [:hello, :clean, :build]
+task :default => [:hello, :clean, :restore, :build]
 
 task :hello do
 	puts 'hello world'
@@ -14,6 +15,16 @@ task :clean do
 	CLEAN.include (FileList["./src/**/obj/"])
 end
 
-task :build do
 
+nugets_restore :restore do |p|
+	p.out = 'src/packages'
+	p.exe = 'tools/Nuget.exe'
+end
+
+desc 'Perform full build'
+build :build => [:restore] do |b|
+	b.sln = 'src/HomePractice.sln'
+	b.target = ['Clean', 'Rebuild', ] 
+	b.prop 'Configuration', 'Release'
+  # alt: b.file = 'src/MyProj.sln'
 end
