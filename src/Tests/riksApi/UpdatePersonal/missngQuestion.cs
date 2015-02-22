bf97e710b8d2b58036f0ab8c80fc1ae72cc4f7d4
Raja Thapa <raja.thapa@comparethemarket.com>
2015-02-22 20:20:32 +0000

    minor renaming

--- src/Tests/riksApi/UpdatePersonal/missngQuestion.cs
+++ src/Tests/riksApi/UpdatePersonal/missngQuestion.cs
@@ -30,7 +30,6 @@ namespace Tests.riksApi
             var requestUri = string.Format("api/risk/{0}/sections/personal", riskId);
             var response =  _client.PostAsJsonAsync(requestUri, peronalDetails).Result;
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
-            var result = response.Content.ReadAsStringAsync().Result;
         }
 
         private void SetUpOwinHost()
